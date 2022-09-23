d066914b8620c9229ccfeec4e53c8c51088853e5
Marius Hartmann <marius.hartmann@e-go-mobile.com>
2022-09-23 21:41:03 +0200

    Fixed storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,40 +1,6 @@
 module.exports = {
-    "stories": [
-        "../src/**/*.stories.mdx",
-        "../src/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
-    "addons": [
-        "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/addon-interactions"
-    ],
-    "framework": "@storybook/html",
-    "core": {
-        "builder": "@storybook/builder-webpack5"
-    },
-    webpackFinal: async (config, { configType }) => {
-        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-        // You can change the configuration based on that.
-        // 'PRODUCTION' is used when building the static version of storybook.
-
-        // Make whatever fine-grained changes you need
-        config.module.rules.push({
-            test: /\.scss$/,
-            exclude: /node_modules/,
-            use: [
-                "sass-to-string",
-                {
-                    loader: "sass-loader",
-                    options: {
-                        sassOptions: {
-                            outputStyle: "compressed"
-                        }
-                    }
-                }
-            ]
-        });
-
-        // Return the altered config
-        return config;
-    }
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/html',
+  staticDirs: ['../dist'],
 };
