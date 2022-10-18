d6c5a781b40ff90092baf4ac964ca17ffeba4da1
Marius Hartmann <marius.hartmann@e-go-mobile.com>
2022-10-18 15:16:08 +0200

    Docs, Logo and Theme

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming";
+
+export default create({
+    base: "light",
+    brandTitle: "Magnet-UI React Framework",
+    brandUrl: "https://github.com/mariushartmann/magnet-ui",
+    brandImage: "/Magnet-UI.png"
+});
